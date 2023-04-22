851398e862588c1ac92ac5fa15e023c26580cb4a
JoelRN <joel.rodriguez.nistal@gmail.com>
2023-04-22 16:27:29 +0200

    Corrección ignore

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Drawing;
+
+internal class Program
+{
+    private static string[] imageExtensions = new string[] { "jpg", "jpeg", "png", "gif", "bmp" };
+
+    private static void Main(string[] args)
+    {
+        string[] files = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory);
+        string[] imageFiles;
+
+        foreach (var f in files) {
+            FileInfo fi = new System.IO.FileInfo(f);
+
+            if (imageExtensions.Contains(fi.Extension)) {
+                resizeImage(f);
+            }
+        }
+    }
+
+    protected static void resizeImage(string path) {
+        Bitmap bmp = new Bitmap(path);
+
+        bmp.SetResolution(20, 20);
+
+        //File.WriteAllBytes(path + ".temp", bmp);
+    }
+}
